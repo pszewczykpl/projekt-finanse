6cedfe978d61ba57c46202554a3bd47fe23b6b14
Piotr Szewczyk <pszewczykpl@gmail.com>
2021-02-28 21:20:04 +0100

    Added PaydayLoan Model with -all

---
+++ app/Http/Controllers/PaydayLoanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaydayLoan;
+use Illuminate\Http\Request;
+
+class PaydayLoanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('paydayloans.index', ['name' => 'Victoria']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PaydayLoan  $paydayLoan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PaydayLoan $paydayLoan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PaydayLoan  $paydayLoan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PaydayLoan $paydayLoan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PaydayLoan  $paydayLoan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PaydayLoan $paydayLoan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PaydayLoan  $paydayLoan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PaydayLoan $paydayLoan)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaydayLoanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('chwilowki', PaydayLoanController::class)->only([
+    'index', 'show'
+]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
